7b986c3d438e2f2732668926fb46fe6fd98c003f
mdsanima <marcinrozewski@gmail.com>
2023-02-07 11:43:21 +0100

    feat: first `mdsanima` test terminal command

---
+++ src/mdsanima_cli/__init__.py
@@ -0,0 +1,11 @@
+# Copyritht © 2023 Marcin Różewski MDSANIMA
+
+
+"""Init module for command line tools."""
+
+
+from __future__ import annotations
+
+
+def hello_world():
+    print("Hello World")
---
+++ src/mdsanima_cli/__main__.py
@@ -0,0 +1,13 @@
+# Copyritht © 2023 Marcin Różewski MDSANIMA
+
+
+"""Main module for command line tools."""
+
+
+from __future__ import annotations
+
+from . import hello_world
+
+
+if __name__ == "__main__":
+    hello_world()
