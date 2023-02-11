fe93d2d0b926517cd37821f87a189372e3a834d7
mdsanima <marcinrozewski@gmail.com>
2023-02-12 00:52:18 +0100

    feat: initial parser for pixelart and gifmaker command closes #1

--- src/mdsanima_cli/__init__.py
+++ src/mdsanima_cli/__init__.py
@@ -1,11 +1,32 @@
 # Copyritht © 2023 Marcin Różewski MDSANIMA
 
 
-"""Init module for command line tools."""
+"""Initial main functionality for command line tools."""
 
 
 from __future__ import annotations
 
+import argparse
 
-def hello_world():
-    print("Hello World")
+
+def create_parser():
+    parser = argparse.ArgumentParser(prog="mdsanima")
+    subparsers = parser.add_subparsers()
+
+    pixelart_parser = subparsers.add_parser("pixelart")
+    pixelart_parser.set_defaults(command="pixelart")
+
+    gifmaker_parser = subparsers.add_parser("gifmaker")
+    gifmaker_parser.set_defaults(command="gifmaker")
+
+    return parser
+
+
+def main_cli():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    if args.command == "pixelart":
+        print("Hello from pixelart")
+    elif args.command == "gifmaker":
+        print("Hello from gifmaker")
