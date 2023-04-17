c75e8254c2b3d3cb100cba15823abb3325496a42
mdsanima <marcinrozewski@gmail.com>
2023-04-17 16:22:53 +0200

    feat!: more stats info for `get_directory_info` function

--- src/mdsanima_cli/utils.py
+++ src/mdsanima_cli/utils.py
@@ -1,8 +1,8 @@
-# Copyritht © 2023 Marcin Różewski MDSANIMA
+# Copyright © 2023 Marcin Różewski MDSANIMA
 
 
-"""This module is for several utils that we may be use for command line tools.
-The module may be used in development.
+"""This module provides various utilities that can be used for command-line
+tools. The module is intended for use in development.
 """
 
 
@@ -12,34 +12,53 @@ import os
 
 
 def get_directory_info() -> dict:
-    """Checking the directory and returning information about that."""
+    """Checking the current directory and returning details info about it."""
+
+    # Real path of directory.
     real_path = os.path.realpath(os.curdir)
     directory = os.listdir()
 
-    files_count = len(directory)
-    image_png_count = 0
-    image_jpg_count = 0
-    image_webp_count = 0
-    other_file_count = 0
+    # Directory stats count variables.
+    all_files_count = len(directory)
+    folders_count = 0
+    files_count = 0
+    images_count = 0
+    png_count = 0
+    jpg_count = 0
+    webp_count = 0
+    others_count = 0
 
-    for file in directory:
-        extension = os.path.splitext(file)[-1].lower()
-        if extension == ".png":
-            image_png_count += 1
-        elif extension == ".jpg":
-            image_jpg_count += 1
-        elif extension == ".webp":
-            image_webp_count += 1
+    # Loop for checking stats.
+    for filename in directory:
+        if os.path.isdir(os.path.join(real_path, filename)):
+            folders_count += 1
+        elif filename.endswith(".png"):
+            png_count += 1
+            files_count += 1
+            images_count += 1
+        elif filename.endswith(".jpg"):
+            jpg_count += 1
+            files_count += 1
+            images_count += 1
+        elif filename.endswith(".webp"):
+            webp_count += 1
+            files_count += 1
+            images_count += 1
         else:
-            other_file_count += 1
+            files_count += 1
+            others_count += 1
 
+    # Dictionary stats info.
     directory_info = {
         "real_path": real_path,
+        "all_files_count": all_files_count,
+        "folders_count": folders_count,
         "files_count": files_count,
-        "image_png_count": image_png_count,
-        "image_jpg_count": image_jpg_count,
-        "image_webp_count": image_webp_count,
-        "other_file_count": other_file_count,
+        "images_count": images_count,
+        "png_count": png_count,
+        "jpg_count": jpg_count,
+        "webp_count": webp_count,
+        "others_count": others_count,
     }
 
     return directory_info
