75ec73c0a5e8c9f7061c3e65988de8bd90542f37
mdsanima <marcinrozewski@gmail.com>
2023-04-18 22:53:28 +0200

    feat: print multiple colors in on line for processing files info
    docs: add documentation for all the function in `mprints` module

--- src/mdsanima_cli/mprints.py
+++ src/mdsanima_cli/mprints.py
@@ -18,10 +18,19 @@ mprint = get_complex_color
 def print_cli_info(
     key: str, info: str, bracket_color: int, key_color: int, info_color: int
 ) -> None:
-    """Helper for printing multiple colors info in one line."""
+    """Helper for printing multiple colors info in one line. Aligning the last
+    bracket to the right within a maximum width of 14 characters. You can
+    choose individual colors for the bracket, text inside the bracket, and the
+    last text.
 
-    # Print multiple colors one line.
-    mprint("[", bracket_color, "")
+    Example look: `[MDSANIMA CLI] -> CHECK`
+    """
+
+    # Calculating align spaces.
+    align = len(key) + 1
+
+    # Print multiple colors in one line.
+    mprint("[".rjust(14 - align), bracket_color, "")
     mprint(str(key), key_color, "")
     mprint("]", bracket_color, " -> ")
     mprint(str(info), info_color)
@@ -30,13 +39,37 @@ def print_cli_info(
 def print_cli_data(
     key: str, data: str, bracket_color: int, key_color: int, data_color: int
 ) -> None:
-    """Helper for printing multiple colors data in one line."""
+    """Helper for printing multiple colors data in one line. Aligning the last
+    bracket to the right within a maximum width of 14 characters. You can
+    choose individual colors for the bracket, text inside the bracket, and the
+    last text.
+
+    Example look: `[REAL PATH] /home/mdsanima/dev/mdsanima-cli`
+    """
 
     # Calculating align spaces.
     align = len(key) + 1
 
-    # Print multiple colors one line.
+    # Print multiple colors in one line.
     mprint("[".rjust(14 - align), bracket_color, "")
     mprint(str(key), key_color, "")
     mprint("]", bracket_color, " ")
     mprint(str(data), data_color)
+
+
+def print_cli_proc(process: str, count: str, old: str, new: str) -> None:
+    """Helper for printing multiple colors processing info in one line. The
+    colors have already been selected, you can configure only the text without
+    changing the colors.
+
+    Example look: `[COMPUTING 0001] image.png -> image_pixelart.png`
+    """
+
+    # Print multiple colors in one line.
+    mprint("[", 50, "")
+    mprint(str(process), 37, " ")
+    mprint(str(count), 24, "")
+    mprint("]", 50, " ")
+    mprint(str(old), 40, "")
+    mprint(" -> ", 49, "")
+    mprint(str(new), 34)
