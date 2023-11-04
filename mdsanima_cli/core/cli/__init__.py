c786caf21370577423666bd5ddf3c772d8337f82
Marcin Różewski <marcinrozewski@gmail.com>
2023-11-04 03:53:05 +0100

    chore(enums): command configuration for the subparsers (#92)

--- mdsanima_cli/core/cli/__init__.py
+++ mdsanima_cli/core/cli/__init__.py
@@ -3,7 +3,8 @@
 """This module holds the core command-line `CLI` interface related code."""
 
 
+from mdsanima_cli.core.cli.command import Command
 from mdsanima_cli.core.cli.config import Config
 
 
-__all__ = ["Config"]
+__all__ = ["Command", "Config"]
